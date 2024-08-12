1b4d7499e9af8a8c3ac2bedd0229f1df9f218b45
Andrii Bondarchuk <a.bondarchuk@kdd-digital.de>
2024-08-12 09:59:49 +0300

    update nuget packages

--- Program.cs
+++ Program.cs
@@ -21,7 +21,7 @@ var host = new HostBuilder()
         services.Configure<LoggerFilterOptions>(options =>
         {
             // The Application Insights SDK adds a default logging filter that instructs ILogger to capture only Warning and more severe logs. Application Insights requires an explicit override.
-            // Log levels can also be configured using appsettings.json. For more information, see https://learn.microsoft.com/en-us/azure/azure-monitor/app/worker-service#ilogger-logs
+            // Log levels can also be configured using host.json. For more information, see https://learn.microsoft.com/en-us/azure/azure-monitor/app/worker-service#ilogger-logs
             var toRemove = options.Rules.FirstOrDefault(rule => rule.ProviderName
                 == "Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider");
 
