434a53f3edfdb2c7e3cfc664900c4aa2fc30d92d
Andrii Bondarchuk <a.bondarchuk@kdd-digital.de>
2025-03-06 11:43:59 +0200

    Extend GarminActivityResponse with properties

---
+++ src/FitSyncHub.GarminConnect/Converters/GarminConnectDateTimeConverter.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace FitSyncHub.GarminConnect.Converters;
+
+public class GarminConnectDateTimeConverter : JsonConverter<DateTime>
+{
+    private const string DateFormat = "yyyy-MM-dd HH:mm:ss"; // Matches your format
+
+    public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        if (DateTime.TryParseExact(reader.GetString(), DateFormat, CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime date))
+        {
+            return date;
+        }
+
+        throw new JsonException($"Invalid date format: {reader.GetString()}");
+    }
+
+    public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.ToString(DateFormat));
+    }
+}
--- src/FitSyncHub.GarminConnect/JsonSerializerContexts/GarminConnectCamelCaseSerializerContext.cs
+++ src/FitSyncHub.GarminConnect/JsonSerializerContexts/GarminConnectCamelCaseSerializerContext.cs
@@ -1,10 +1,14 @@
 ﻿using System.Text.Json.Serialization;
+using FitSyncHub.GarminConnect.Converters;
 using FitSyncHub.GarminConnect.Models.Requests;
 using FitSyncHub.GarminConnect.Models.Responses;
 
 namespace FitSyncHub.Functions.JsonSerializerContexts;
 
-[JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase)]
+[JsonSourceGenerationOptions(
+    PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase,
+    Converters = [typeof(GarminConnectDateTimeConverter)]
+    )]
 [JsonSerializable(typeof(GarminActivityUpdateRequest))]
 [JsonSerializable(typeof(GarminSetUserWeightRequest))]
 [JsonSerializable(typeof(GarminActivityResponse[]))]
