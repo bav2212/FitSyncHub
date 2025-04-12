1a830833196a237b718848e18904ce2853e54cc1
Andrii Bondarchuk <a.bondarchuk@kdd-digital.de>
2025-04-12 18:28:23 +0300

    Add helper to merge apple workout file fit file

---
+++ src/FitSyncHub.Common/Applications/Apple/AppleHealthExportFileParser.cs
@@ -0,0 +1,51 @@
+﻿using System.Xml;
+
+namespace FitSyncHub.Common.Applications.Apple;
+
+public static class AppleHealthExportFileParser
+{
+    public static List<AppleHeartRateRecord> LoadHeartRateData(string xmlFilePath)
+    {
+        List<AppleHeartRateRecord> heartRateData = [];
+        var settings = new XmlReaderSettings { DtdProcessing = DtdProcessing.Ignore, IgnoreWhitespace = true };
+
+        using (var reader = XmlReader.Create(xmlFilePath, settings))
+        {
+            while (reader.Read())
+            {
+                if (reader.NodeType == XmlNodeType.Element && reader.Name == "Record"
+                    && reader.GetAttribute("type") == "HKQuantityTypeIdentifierHeartRate")
+                {
+                    var timeString = reader.GetAttribute("startDate");
+                    var valueString = reader.GetAttribute("value");
+
+                    if (DateTime.TryParse(timeString, out var time) && int.TryParse(valueString, out var heartRate))
+                    {
+                        heartRateData.Add(new AppleHeartRateRecord
+                        {
+                            Time = time.ToUniversalTime(),
+                            HeartRate = heartRate
+                        });
+                    }
+                }
+            }
+        }
+
+        return heartRateData;
+    }
+
+    public static int? FindClosestHeartRate(List<AppleHeartRateRecord> heartRates, DateTime trackTime)
+    {
+        var closest = heartRates
+            .OrderBy(hr => Math.Abs((hr.Time - trackTime).TotalSeconds)) // Find nearest timestamp
+            .FirstOrDefault();
+
+        return closest?.HeartRate > 0 ? closest.HeartRate : null;
+    }
+}
+
+public record AppleHeartRateRecord
+{
+    public required DateTime Time { get; init; }
+    public required int HeartRate { get; init; }
+}
---
+++ src/FitSyncHub.Functions/Functions/MergeFitWithAppleWorkoutHttpTriggerFunction.cs
@@ -0,0 +1,61 @@
+﻿using FitSyncHub.Common.Applications.Apple;
+using FitSyncHub.Common.Fit;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+
+namespace FitSyncHub.Functions.Functions;
+
+public class MergeFitWithAppleWorkoutHttpTriggerFunction
+{
+    private readonly FitFileDecoder _decoder;
+    private readonly FitFileEncoder _encoder;
+
+    public MergeFitWithAppleWorkoutHttpTriggerFunction(
+        FitFileDecoder decoder,
+        FitFileEncoder encoder)
+    {
+        _decoder = decoder;
+        _encoder = encoder;
+    }
+
+#if DEBUG
+    [Function(nameof(MergeFitWithAppleWorkoutHttpTriggerFunction))]
+#endif
+    public async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Function, "get", Route = "merge-fit-with-apple")] HttpRequest req,
+        CancellationToken cancellationToken)
+    {
+        string? fitPath = req.Query["fitPath"];
+        string? appleExportPath = req.Query["appleExportPath"];
+
+        if (string.IsNullOrWhiteSpace(fitPath) || string.IsNullOrWhiteSpace(appleExportPath))
+        {
+            return new BadRequestObjectResult("wrong request");
+        }
+
+        var heartRateData = AppleHealthExportFileParser.LoadHeartRateData(appleExportPath);
+
+        heartRateData = [.. heartRateData.Where(x => x.Time > DateTime.UtcNow.AddDays(-1))];
+
+        var fitFileMessages = _decoder.Decode(fitPath);
+
+        foreach (var recordMessage in fitFileMessages.RecordMesgs)
+        {
+            var timestamp = recordMessage.GetTimestamp().GetDateTime();
+
+            var heartRate = AppleHealthExportFileParser.FindClosestHeartRate(
+                heartRateData,
+                timestamp);
+
+            recordMessage.SetHeartRate((byte)heartRate);
+        }
+
+        await using var memoryStream = new MemoryStream();
+        _encoder.Encode(memoryStream, fitFileMessages);
+
+        File.WriteAllBytes(@"C:\Users\bav22\Downloads\merged.fit", memoryStream.ToArray());
+
+        return new OkObjectResult("Success");
+    }
+}
