834b7f9e4f2bfe454c26eb281a0f139557d745e6
Andrii Bondarchuk <a.bondarchuk@kdd-digital.de>
2025-10-13 12:55:23 +0300

    Tiny cleanup

--- src/FitSyncHub.Functions/Functions/GarminWorkoutUploadToStravaHttpTriggerFunction.cs
+++ src/FitSyncHub.Functions/Functions/GarminWorkoutUploadToStravaHttpTriggerFunction.cs
@@ -156,7 +156,6 @@ public class GarminWorkoutUploadToStravaHttpTriggerFunction
             {
                 sb.AppendLine($"Activity '{activityName}' uploaded to Intervals.icu");
             }
-
         }
 
         return new OkObjectResult(sb.ToString());
