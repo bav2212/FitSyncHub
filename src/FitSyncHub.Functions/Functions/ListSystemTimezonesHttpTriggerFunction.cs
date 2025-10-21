1fab9d929653a55f70a5b4e258f6b3ef5696a46d
Andrii Bondarchuk <a.bondarchuk@kdd-digital.de>
2025-10-21 10:38:59 +0300

    Add new function

---
+++ src/FitSyncHub.Functions/Functions/ListSystemTimezonesHttpTriggerFunction.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+
+namespace FitSyncHub.Functions.Functions;
+
+public class ListSystemTimezonesHttpTriggerFunction
+{
+    [Function(nameof(ListSystemTimezonesHttpTriggerFunction))]
+    public async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Function, "get", Route = "list-time-zones")] HttpRequest req)
+    {
+        _ = req;
+
+        return new OkObjectResult(TimeZoneInfo.GetSystemTimeZones());
+    }
+}
