7e18ad6fb9d1bb72b70ed1dfaf5c7fdbec612980
Andrii Bondarchuk <a.bondarchuk@kdd-digital.de>
2025-10-24 16:15:10 +0300

    ZwiftHttpClient: add new endpoint

--- src/FitSyncHub.Zwift/HttpClients/ZwiftHttpClient.cs
+++ src/FitSyncHub.Zwift/HttpClients/ZwiftHttpClient.cs
@@ -17,6 +17,20 @@ public partial class ZwiftHttpClient
         _logger = logger;
     }
 
+    public async Task<PlayerProfile> GetProfileMe(CancellationToken cancellationToken)
+    {
+        var request = new HttpRequestMessage(HttpMethod.Get, "api/profiles/me");
+        request.Headers.Add("Accept-Encoding", "gzip, deflate");
+        request.Headers.Add("Accept", "application/x-protobuf-lite");
+
+        var response = await _httpClient.SendAsync(request, cancellationToken);
+        response.EnsureSuccessStatusCode();
+
+        var stream = await response.Content.ReadAsStreamAsync(cancellationToken);
+
+        return PlayerProfile.Parser.ParseFrom(stream);
+    }
+
     public async Task<List<int>> GetAchievements(CancellationToken cancellationToken)
     {
         const string Url = "/achievement/loadPlayerAchievements";
