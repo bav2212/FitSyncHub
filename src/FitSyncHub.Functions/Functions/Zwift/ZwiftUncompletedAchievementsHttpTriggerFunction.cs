22eeeea4a64531a74f8cca6a512b6eda92160145
Andrii Bondarchuk <bav2212@gmail.com>
2026-07-07 13:12:51 +0300

    Refactor achievement summary formatting and logic
    Improve formatting and logic for displaying unachieved general and cycling route achievements in ZwiftUncompletedAchievementsHttpTriggerFunction. Add FormatRoutesSummary helper for route summaries, enhance indentation, and separate summary logic for better readability and maintainability.

--- src/FitSyncHub.Functions/Functions/Zwift/ZwiftUncompletedAchievementsHttpTriggerFunction.cs
+++ src/FitSyncHub.Functions/Functions/Zwift/ZwiftUncompletedAchievementsHttpTriggerFunction.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text;
+using FitSyncHub.Zwift.Models;
 using FitSyncHub.Zwift.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -43,16 +44,18 @@ public class ZwiftUncompletedAchievementsHttpTriggerFunction
             // Exclude achievements that are not possible to achieve anymore
             generalAchievements = [.. generalAchievements.Where(x => x.Achievement.Id is < 347 or > 471)];
 
-            sb.AppendLine($"Achieved (excluding running and not possible to achieve anymore): {generalAchievements.Count(x => x.IsAchieved)}/{generalAchievements.Count}");
+            sb.AppendLine($"\tAchieved (excluding running and not possible to achieve anymore): {generalAchievements.Count(x => x.IsAchieved)}/{generalAchievements.Count}");
 
-            foreach (var generalAchievementState in generalAchievements)
+            var unachievedGeneralAchievements = generalAchievements.Where(x => !x.IsAchieved).ToList();
+
+            if (unachievedGeneralAchievements.Count != 0)
             {
-                if (generalAchievementState.IsAchieved)
-                {
-                    continue;
-                }
+                sb.AppendLine($"\tUnachieved:");
+            }
 
-                sb.AppendLine($"- {generalAchievementState.Achievement.Name}");
+            foreach (var unachievedGeneralAchievement in unachievedGeneralAchievements)
+            {
+                sb.AppendLine($"\t- {unachievedGeneralAchievement.Achievement.Name}");
             }
             sb.AppendLine();
         }
@@ -68,40 +71,59 @@ public class ZwiftUncompletedAchievementsHttpTriggerFunction
                 })
                 .ToDictionary(x => x.Key.PublicEventsOnly, g => new
                 {
-                    IsAchievedCount = g.Count(x => x.Key.IsAchieved),
-                    Count = g.Count()
+                    NonAchievedItems = g.Where(x => !x.Key.IsAchieved).Select(x => x.Value).ToList(),
+                    TotalCount = g.Count(),
+
                 });
 
-            sb.AppendLine($"Achieved (public routes): {statsLookup[false].IsAchievedCount}/{statsLookup[false].Count}");
-            sb.AppendLine($"Achieved (eventOnly routes): {statsLookup[true].IsAchievedCount}/{statsLookup[true].Count}");
+            sb.AppendLine("Public Routes:");
+            sb.AppendLine(FormatRoutesSummary(publicEventsOnly: false));
 
-            var uncompletedRoutesOrdered = achievementsState.CyclingRouteAchievementsToRouteMapping
-                .Where(x => !x.Key.IsAchieved)
-                .Select(x => x.Value)
-                .OrderBy(x => x.PublicEventsOnly)
-                .ThenBy(x => x.PublishedOn)
-                .ToList();
+            sb.AppendLine();
 
-            foreach (var route in uncompletedRoutesOrdered)
+            sb.AppendLine("EventOnly Routes:");
+            sb.AppendLine(FormatRoutesSummary(publicEventsOnly: true));
+
+            string FormatRoutesSummary(bool publicEventsOnly)
             {
-                var totalDistanceKm = Math.Round(route.TotalDistanceInMeters / 1000.0, 1);
-                var totalElevation = Math.Round(route.TotalAscentInMeters, 0);
+                var lookupValue = statsLookup[publicEventsOnly];
 
-                sb.Append($"- {route.Name} ({totalDistanceKm}km, {totalElevation}m)");
-                if (route.PublicEventsOnly)
-                {
-                    sb.Append(", events only");
-                }
-                if (route.PublishedOn.HasValue && route.PublishedOn > DateOnly.FromDateTime(DateTime.Today))
-                {
-                    sb.Append($", will published on {route.PublishedOn:yyyy-MM-dd}");
-                }
-                sb.AppendLine();
+                return lookupValue is not null
+                    ? ZwiftUncompletedAchievementsHttpTriggerFunction.FormatRoutesSummary(statsLookup[publicEventsOnly].NonAchievedItems, statsLookup[publicEventsOnly].TotalCount)
+                    : "";
+            }
+        }
 
+        return new OkObjectResult(sb.ToString());
+    }
+
+    private static string FormatRoutesSummary(List<ZwiftRouteModel> nonAchievedItems, int totalCount)
+    {
+        StringBuilder sb = new();
+
+        sb.AppendLine($"\tAchieved: {totalCount - nonAchievedItems.Count}/{totalCount}");
+        if (nonAchievedItems.Count != 0)
+        {
+            sb.AppendLine($"\tUnachieved:");
+        }
+
+        foreach (var route in nonAchievedItems.OrderBy(x => x.PublishedOn))
+        {
+            var totalDistanceKm = Math.Round(route.TotalDistanceInMeters / 1000.0, 1);
+            var totalElevation = Math.Round(route.TotalAscentInMeters, 0);
+
+            sb.Append($"\t- {route.Name} ({totalDistanceKm}km, {totalElevation}m)");
+            if (route.PublicEventsOnly)
+            {
+                sb.Append(", events only");
+            }
+            if (route.PublishedOn.HasValue && route.PublishedOn > DateOnly.FromDateTime(DateTime.Today))
+            {
+                sb.Append($", will published on {route.PublishedOn:yyyy-MM-dd}");
             }
             sb.AppendLine();
         }
 
-        return new OkObjectResult(sb.ToString());
+        return sb.ToString();
     }
 }
