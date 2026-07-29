a18a0582c4833b65bb605e26a7077a035437b181
Andrii Bondarchuk <bav2212@gmail.com>
2026-07-29 15:15:54 +0300

    Add ZwiftProfileInfoHttpTriggerFunction HTTP GET endpoint
    Added Azure Function "zwift-profile-info" to fetch Zwift profile info by "profileId" query parameter. Handles validation, not found, and returns profile data using ZwiftHttpClient.

---
+++ src/FitSyncHub.Functions/Functions/Zwift/ZwiftProfileInfoHttpTriggerFunction.cs
@@ -0,0 +1,47 @@
+﻿using FitSyncHub.Zwift.HttpClients;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace FitSyncHub.Functions.Functions.Zwift;
+
+public sealed class ZwiftProfileInfoHttpTriggerFunction
+{
+    private readonly ZwiftHttpClient _zwiftHttpClient;
+    private readonly ILogger<ZwiftProfileInfoHttpTriggerFunction> _logger;
+
+    public ZwiftProfileInfoHttpTriggerFunction(
+        ZwiftHttpClient zwiftHttpClient,
+        ILogger<ZwiftProfileInfoHttpTriggerFunction> logger)
+    {
+        _zwiftHttpClient = zwiftHttpClient;
+        _logger = logger;
+    }
+
+    [Function(nameof(ZwiftProfileInfoHttpTriggerFunction))]
+    public async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Function, "get", Route = "zwift-profile-info")] HttpRequest req,
+        CancellationToken cancellationToken)
+    {
+        var profileIdQueryParam = req.Query["profileId"];
+        if (string.IsNullOrWhiteSpace(profileIdQueryParam))
+        {
+            return new BadRequestObjectResult("Specify params: profileId");
+        }
+
+        if (!long.TryParse(profileIdQueryParam, out var profileId)
+            || profileId <= 0)
+        {
+            return new BadRequestObjectResult("profileId is not valid long number");
+        }
+
+        var profile = await _zwiftHttpClient.GetProfileDetailed(profileId, cancellationToken);
+        if (profile is null)
+        {
+            return new NotFoundObjectResult($"Profile with id {profileId} not found");
+        }
+
+        return new OkObjectResult(profile);
+    }
+}
