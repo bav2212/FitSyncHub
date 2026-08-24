a95c1cb3a659ff0664db3672e627db6d64b7e150
Andrii Bondarchuk <bav2212@gmail.com>
2026-08-24 14:54:23 +0300

    Set ZwiftListActivitiesRequest.Limit default to 20
    Changed the default value of the Limit property in ZwiftListActivitiesRequest from 0 to 20. Now, if not set, Limit will default to 20.

--- src/FitSyncHub.Zwift/HttpClients/Models/Requests/Activities/ZwiftListActivitiesRequest.cs
+++ src/FitSyncHub.Zwift/HttpClients/Models/Requests/Activities/ZwiftListActivitiesRequest.cs
@@ -4,5 +4,5 @@ public sealed record ZwiftListActivitiesRequest
 {
     public required long ProfileId { get; init; }
     public int Start { get; init; } = 0;
-    public int Limit { get; init; } = 0;
+    public int Limit { get; init; } = 20;
 }
